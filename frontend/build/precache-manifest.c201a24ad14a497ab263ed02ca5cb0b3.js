d8538e9b394a2e9d1594dfe39ef774663f520bf4
salaah-community <117308467+salaah-community@users.noreply.github.com>
2023-01-30 07:46:33 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.c201a24ad14a497ab263ed02ca5cb0b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "51c41953a0e6f4cc679b",
+    "url": "/static/css/main.877ed165.chunk.css"
+  },
+  {
+    "revision": "51c41953a0e6f4cc679b",
+    "url": "/static/js/main.bd1c7268.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7d516d7de3903680db26",
+    "url": "/static/js/2.e136b140.chunk.js"
+  },
+  {
+    "revision": "24f0d35740e86109a51e7edce426d858",
+    "url": "/static/media/logo2.24f0d357.png"
+  },
+  {
+    "revision": "25e7373283de2fbe972331938ca7d35a",
+    "url": "/static/media/9.25e73732.jpg"
+  },
+  {
+    "revision": "b41333338df053374d00613575384802",
+    "url": "/static/media/6.b4133333.mp4"
+  },
+  {
+    "revision": "b01604a1144f5614d1b018d409541201",
+    "url": "/index.html"
+  }
+];
