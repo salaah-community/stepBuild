ecb65f83a6da43792a8965de678798080a480964
salaah-community <117308467+salaah-community@users.noreply.github.com>
2023-03-18 22:59:54 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.11bb917368b9f5db1eeb8b1bfba875a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "56d5899d324ec86c3f1f",
+    "url": "/static/css/main.c46ef7ae.chunk.css"
+  },
+  {
+    "revision": "56d5899d324ec86c3f1f",
+    "url": "/static/js/main.9ebc0106.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d6fbc6c64f1c187ba793",
+    "url": "/static/js/2.c3e4a1d7.chunk.js"
+  },
+  {
+    "revision": "24f0d35740e86109a51e7edce426d858",
+    "url": "/static/media/logo2.24f0d357.png"
+  },
+  {
+    "revision": "25e7373283de2fbe972331938ca7d35a",
+    "url": "/static/media/9.25e73732.jpg"
+  },
+  {
+    "revision": "b41333338df053374d00613575384802",
+    "url": "/static/media/6.b4133333.mp4"
+  },
+  {
+    "revision": "0bd229a388a861c9437a5d70d59976a8",
+    "url": "/static/media/STEPCalender.0bd229a3.pdf"
+  },
+  {
+    "revision": "d45e02a9b5c5efc855c05cd294142271",
+    "url": "/index.html"
+  }
+];
